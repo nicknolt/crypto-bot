3130d4369c14b13c769cec59714f9bce1d30ab43
stevedidienne <steve.didienne@espci.psl.eu>
2021-09-20 13:38:38 +0200

    1er commit

---
+++ BackTest.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from binance.client import Client
+import ta
+import pandas_ta as pda
+import matplotlib.pyplot as plt
+import numpy as np
+from termcolor import colored
+
+client = Client()
+
+klinesT = client.get_historical_klines("BTCBUSD", Client.KLINE_INTERVAL_15MINUTE, "01 may 2021")
+
+df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+df['close'] = pd.to_numeric(df['close'])
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+df['open'] = pd.to_numeric(df['open'])
+
+df = df.set_index(df['timestamp'])
+df.index = pd.to_datetime(df.index, unit='ms')
+del df['timestamp']
+print(df)
+
+# ----- Functions Definition -------
+def get_chop(high, low, close, window):
+    tr1 = pd.DataFrame(high - low).rename(columns = {0:'tr1'})
+    tr2 = pd.DataFrame(abs(high - close.shift(1))).rename(columns = {0:'tr2'})
+    tr3 = pd.DataFrame(abs(low - close.shift(1))).rename(columns = {0:'tr3'})
+    frames = [tr1, tr2, tr3]
+    tr = pd.concat(frames, axis = 1, join = 'inner').dropna().max(axis = 1)
+    atr = tr.rolling(1).mean()
+    highh = high.rolling(window).max()
+    lowl = low.rolling(window).min()
+    ci = 100 * np.log10((atr.rolling(window).sum()) / (highh - lowl)) / np.log10(window)
+    return ci
+
+
+# -----------------------------------------------------------------
+
+df.drop(df.columns.difference(['open','high','low','close','volume']), 1, inplace=True)
+
+#Simple Moving Average
+# df['SMA']=ta.trend.sma_indicator(df['close'], window=12)
+
+#Exponential Moving Average
+df['EMA1']=ta.trend.ema_indicator(close=df['close'], window=13)
+df['EMA2']=ta.trend.ema_indicator(close=df['close'], window=38)
+
+# #Relative Strength Index (RSI)
+# df['RSI'] =ta.momentum.rsi(close=df['close'], window=14)
+
+# #MACD
+# MACD = ta.trend.MACD(close=df['close'], window_fast=12, window_slow=26, window_sign=9)
+# df['MACD'] = MACD.macd()
+# df['MACD_SIGNAL'] = MACD.macd_signal()
+# df['MACD_DIFF'] = MACD.macd_diff() #Histogramme MACD
+
+# #Stochastic RSI
+# df['STOCH_RSI'] = ta.momentum.stochrsi(close=df['close'], window=14, smooth1=3, smooth2=3) #Non moyenné
+# df['STOCH_RSI_D'] = ta.momentum.stochrsi_d(close=df['close'], window=14, smooth1=3, smooth2=3) #Orange sur TradingView
+# df['STOCH_RSI_K'] =ta.momentum.stochrsi_k(close=df['close'], window=14, smooth1=3, smooth2=3) #Bleu sur TradingView
+
+# #Ichimoku
+# df['KIJUN'] = ta.trend.ichimoku_base_line(high=df['high'], low=df['low'], window1=9, window2=26)
+# df['TENKAN'] = ta.trend.ichimoku_conversion_line(high=df['high'], low=df['low'], window1=9, window2=26)
+# df['SSA'] = ta.trend.ichimoku_a(high=df['high'], low=df['low'], window1=9, window2=26)
+# df['SSB'] = ta.trend.ichimoku_b(high=df['high'], low=df['low'], window2=26, window3=52)
+
+# #Bollinger Bands
+# BOL_BAND = ta.volatility.BollingerBands(close=df['close'], window=20, window_dev=2)
+# df['BOL_H_BAND'] = BOL_BAND.bollinger_hband() #Bande Supérieur
+# df['BOL_L_BAND'] = BOL_BAND.bollinger_lband() #Bande inférieur
+# df['BOL_MAVG_BAND'] = BOL_BAND.bollinger_mavg() #Bande moyenne
+
+# #Average True Range (ATR)
+# df['ATR'] = ta.volatility.average_true_range(high=df['high'], low=df['low'], close=df['close'], window=14)
+
+# #Super Trend
+# ST_length = 10
+# ST_multiplier = 3.0
+# superTrend = pda.supertrend(high=df['high'], low=df['low'], close=df['close'], length=ST_length, multiplier=ST_multiplier)
+# df['SUPER_TREND'] = superTrend['SUPERT_'+str(ST_length)+"_"+str(ST_multiplier)] #Valeur de la super trend
+# df['SUPER_TREND_DIRECTION'] = superTrend['SUPERTd_'+str(ST_length)+"_"+str(ST_multiplier)] #Retourne 1 si vert et -1 si rouge
+
+# #Awesome Oscillator
+# df['AWESOME_OSCILLATOR'] = ta.momentum.awesome_oscillator(high=df['high'], low=df['low'], window1=5, window2=34)
+
+# # Kaufman’s Adaptive Moving Average (KAMA)
+# df['KAMA'] = ta.momentum.kama(close=df['close'], window=10, pow1=2, pow2=30)
+
+# #Choppiness index
+# df['CHOP'] = get_chop(high=df['high'], low=df['low'], close=df['close'], window=14)
+
+df
